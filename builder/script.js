6738f24bd0f1de65401b46edf6a34ef54237d1a3
Nihal Baranwal <nihalkumar9749@gmail.com>
2025-02-24 12:42:41 +0530

    feat(builder): uploading build files to S3

--- builder/script.js
+++ builder/script.js
@@ -1,5 +1,18 @@
 import { exec } from "child_process";
 import path from "path";
+import fs from "fs";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+import mime from "mime-types";
+
+const s3Client = new S3Client({
+    region: '',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: '',
+    }
+});
+
+const PROJECT_ID = process.env.PROJECT_ID;
 
 async function init() {
     console.log("Executing script");
@@ -21,7 +34,34 @@ async function init() {
     });
 
     // When process gets finished
-    p.on("close", () => {
+    p.on("close", async () => {
         console.log("Build completed");
+        // The build files will be present in "output/dist" folder
+        const distFolderPath = path.join(__dirname, "output", "dist");
+
+        // Read the contents of the folder, recursive true means it will go keep going inside directories
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true });
+
+        // Loop over the file paths
+        for(const filePath of distFolderContents) {
+            // If path is of a directory continue. Since inside the dist folder there can be directories like "assets" which contains media files.
+            if(fs.lstatSync(filePath).isDirectory()) continue;
+
+            const command = new PutObjectCommand({
+                //bucket name
+                Bucket: '',
+                //Path in the bucket where to store the file
+                Key: `__outputs/${PROJECT_ID}/${filePath}`,
+                //actual content of the file
+                Body: fs.createReadStream(filePath),
+                //Dynamically get content type of file
+                ContentType: mime.lookup(filePath),
+            });
+
+            //Send the command we created
+            await s3Client.send(command);
+        }
+
+        console.log("Done...");
     });
 }
