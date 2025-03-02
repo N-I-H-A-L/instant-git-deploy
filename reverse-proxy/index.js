6f4fe82bdf9f735bf185912c0e77fb27a52e49b2
Nihal Baranwal <nihalkumar9749@gmail.com>
2025-03-02 22:01:15 +0530

    feat(proxy): reverse proxy for serving projects

---
+++ reverse-proxy/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import httpProxy from "http-proxy";
+
+const app = express();
+const PORT = 8000;
+
+const BASE_PATH = "https://instant-git-deploy.s3.ap-south-1.amazonaws.com/__outputs";
+
+const proxy = httpProxy.createProxy();
+
+//Catch all requests
+app.use((req, res) => {
+    //Get the hostname or URL of request
+    const hostname = req.hostname;
+
+    //Get the first domain (project ID)
+    const subdomain = hostname.split('.')[0];
+
+    //URL of HTML file in bucket
+    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+    const url = req.url;
+    if(url === "/"){
+        proxyReq.path += "index.html";
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Reverse proxy is running on ${PORT}`);
+});
